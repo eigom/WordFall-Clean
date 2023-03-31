7cb606c210b76bf38b06ea36f6606336e3bbada9
Eigo Madaloja <eigo.madaloja@gmail.com>
2023-03-31 10:05:11 +0300

    Initial Commit

---
+++ WordFall-Clean/WordFall_CleanApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordFall_CleanApp.swift
+//  WordFall-Clean
+//
+//  Created by Eigo Madaloja on 31.03.2023.
+//
+
+import SwiftUI
+
+@main
+struct WordFall_CleanApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
