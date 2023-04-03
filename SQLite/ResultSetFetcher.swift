a009bd33aee65a5562a9ad5a609001f423b23bf0
Eigo Madaloja <eigo.madaloja@gmail.com>
2023-04-03 15:13:39 +0300

    ResultSetFetcher update

--- SQLite/ResultSetFetcher.swift
+++ SQLite/ResultSetFetcher.swift
@@ -2,9 +2,8 @@ import SQLite3
 
 final class ResultSetFetcher {
     public enum Error: Swift.Error {
-        case failedToGetColumnName
         case failedToPrepareQuery(String, errorCode: Int32)
-        case failedToEvaluateQuery(String, errorCode: Int32)
+        case invalidStatementHandle
     }
 
     private let databaseHandle: OpaquePointer
@@ -24,26 +23,10 @@ final class ResultSetFetcher {
 
         defer { sqlite3_finalize(statement) }
 
-        let columnNames = try columnNames(in: statement)
-        let resultSet = ResultSet(columnNames: columnNames)
+        guard let statement = statement else { throw Error.invalidStatementHandle }
 
-        while true {
-            let stepResult = sqlite3_step(statement)
-            guard stepResult != SQLITE_DONE else { return resultSet }
-            guard stepResult == SQLITE_ROW else { throw Error.failedToEvaluateQuery(query, errorCode: stepResult) }
-        }
-    }
-
-    private func columnNames(in statement: OpaquePointer?) throws  -> [String] {
-        let columnCount = sqlite3_column_count(statement)
-
-        return try (0 ..< columnCount).reduce([String]()) { columnNames, index in
-            guard let name = sqlite3_column_name(statement, 0) else { throw Error.failedToGetColumnName }
-            return columnNames + [String(cString: name)]
-        }
-    }
-
-    private func row(from statement: OpaquePointer?) -> ResultSet.Row {
-
+        let resultBuilder = ResultSetBuilder(statement: statement)
+        
+        return try resultBuilder.build()
     }
 }
