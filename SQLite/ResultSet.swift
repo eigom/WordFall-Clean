6f5fb3d83e6ce6e41b8e133711f3fbe82d5a79f9
Eigo Madaloja <eigo.madaloja@gmail.com>
2023-04-03 16:35:33 +0300

    added asJSONString

--- SQLite/ResultSet.swift
+++ SQLite/ResultSet.swift
@@ -6,6 +6,10 @@ struct ResultSet {
         case unsupported
     }
 
+    enum Error: Swift.Error {
+        case unsupportedValue
+    }
+
     typealias Row = [Value]
     
     let columnNames: [String]
@@ -16,4 +20,40 @@ struct ResultSet {
     }
 
     static let empty = ResultSet(columnNames: [], rows: [])
+}
+
+extension ResultSet {
+    var asJSONString: String {
+        get throws {
+            let rows = try rows.reduce([String]()) { partialResult, row in
+                partialResult + [try makeJSON(for: row)]
+            }
+            
+            return "{" + rows.joined(separator: ", ") + "}"
+        }
+    }
+
+    private func makeJSON(for row: Row) throws -> String {
+        let values = try row.enumerated().reduce([String]()) { partialResult, value in
+            let name = "\"" + columnNames[value.offset] + "\""
+            let value = try makeJSON(for: value.element)
+            return partialResult + ["\(name): \(value)"]
+        }
+
+        return "[" + values.joined(separator: ", ") + "]"
+    }
+
+    private func makeJSON(for value: Value) throws -> String {
+        switch value {
+        case .null:
+            return "null"
+        case .integer(let value):
+            return String(value)
+        case .string(let value):
+            let escapedValue = value.replacing("\"", with: "\\\"")
+            return "\"" + escapedValue + "\""
+        case .unsupported:
+            throw Error.unsupportedValue
+        }
+    }
 }
