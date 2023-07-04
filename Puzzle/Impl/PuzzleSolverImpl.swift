487d9947d69746ab57bd0b35f7825c5bf05a0058
Eigo Madaloja <eigo.madaloja@gmail.com>
2023-07-04 12:10:35 +0300

    solve impl

--- Puzzle/Impl/PuzzleSolverImpl.swift
+++ Puzzle/Impl/PuzzleSolverImpl.swift
@@ -1,31 +1,11 @@
 public struct PuzzleSolverImpl: PuzzleSolver {
     public func solve(_ puzzle: WordPuzzle, using letterRevealer: PuzzleLetterRevealer) -> (WordPuzzle, [PuzzleLetter]) {
-        puzzle.puzzleLetters.reduce(<#T##initialResult: Result##Result#>, <#T##nextPartialResult: (Result, Character?) throws -> Result##(Result, Character?) throws -> Result##(_ partialResult: Result, Character?) throws -> Result#>)
-
-        let revealed = puzzle.puzzleLetters
-            .enumerated()
-            .map { letterRevealer.revealLetter(at: <#T##Int#>, in: <#T##WordPuzzle#>) }
-
-        /*let revealedLetters = puzzle.solutionLetters
-            .enumerated()
-            .filter { $0.element == nil }
-            .map {
-                SolvePuzzleResult.RevealedLetter(
-                    letter: puzzle.wordLetters[$0.offset],
-                    solutionLetterIndex: $0.offset
-                )
+        puzzle.puzzleLetters
+            .reduce((puzzle, [PuzzleLetter]())) { partialResult, _ in
+                let (puzzle, letters) = partialResult
+                let (newPuzzle, revealedLetter) = letterRevealer.revealLetter(at: letters.count, in: puzzle)
+                return (newPuzzle, letters + [revealedLetter].compactMap { $0 })
             }
-        let newPuzzleLetters = puzzle.puzzleLetters.map { _ -> Character? in nil }
-        let newSolutionLetters = puzzle.wordLetters
-        let solvedPuzzle = puzzle.copy(
-            puzzleLetters: newPuzzleLetters,
-            solutionLetters: newSolutionLetters
-        )
-
-        return SolvePuzzleResult(
-            revealedLetters: revealedLetters,
-            resultingPuzzle: solvedPuzzle
-        )*/
     }
 
     public func isSolved(_ puzzle: WordPuzzle) -> Bool {
