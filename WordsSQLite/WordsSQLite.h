76c44693c31e16cfd1cdd2d63cb87132259652b6
Eigo Madaloja <eigo.madaloja@gmail.com>
2023-07-07 11:42:19 +0300

    WordsSQLite module

---
+++ WordsSQLite/WordsSQLite.h
@@ -0,0 +1,18 @@
+//
+//  WordsSQLite.h
+//  WordsSQLite
+//
+//  Created by Eigo Madaloja on 07.07.2023.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for WordsSQLite.
+FOUNDATION_EXPORT double WordsSQLiteVersionNumber;
+
+//! Project version string for WordsSQLite.
+FOUNDATION_EXPORT const unsigned char WordsSQLiteVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <WordsSQLite/PublicHeader.h>
+
+
