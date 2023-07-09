173084fb24a55a7b4855e9963c1af4ca7318cbb7
Eigo Madaloja <eigo.madaloja@gmail.com>
2023-07-09 12:41:46 +0300

    settings update

--- Settings/Setting.swift
+++ Settings/Setting.swift
@@ -1,38 +1,3 @@
-/*public protocol BooleanSettingStorage {
-    func value(for identifier: String) -> Bool?
-    func store(_ value: Bool, for identifier: String)
-}
-
-public protocol StringSettingStorage {
-    func value(for identifier: String) -> String?
-    func store(_ value: String, for identifier: String)
-}
-
-class UserDefaultsSettingStorage: BooleanSettingStorage, StringSettingStorage {
-    func value(for identifier: String) -> Bool? {
-        return true
-    }
-
-    func store(_ value: Bool, for identifier: String) {
-
-    }
-
-    func value(for identifier: String) -> String? {
-        return ""
-    }
-
-    func store(_ value: String, for identifier: String) {
-
-    }
-}*/
-
-public protocol SettingStorage {
-    associatedtype ValueType
-
-    func value(for identifier: String) -> ValueType?
-    func store(_ value: ValueType, for identifier: String)
-}
-
 public protocol Setting {
     associatedtype ValueType
 
@@ -42,87 +7,70 @@ public protocol Setting {
     func removeObserver(_ observer: AnyObject)
 }
 
-/*public protocol BooleanSetting: Setting where ValueType == Bool {
-    init(identifier: String, defaultValue: Bool)
-}*/
+public protocol SettingStorage {
+    associatedtype ValueType
+
+    func value(for identifier: String) -> ValueType?
+    func store(_ value: ValueType, for identifier: String)
+}
 
 import Common
 
-public class SettingImpl<V, Storage: SettingStorage, Notifier: ObserverNotifier>: Setting
-        where Storage.ValueType == V, Notifier.Notification == V {
+public protocol BooleanSettingStorage: SettingStorage where ValueType == Bool {}
+
+public protocol BooleanSettingUpdateNotifier: ObserverNotifier where Notification == Bool {}
+
+public class BooleanSetting: Setting {
     private let identifier: String
-    private let storage: Storage
-    private let notifier: Notifier
-
-    public var value: V {
-        didSet {
-            storage.store(value, for: identifier)
-            notifier.notify(value)
-        }
-    }
-
-    init(identifier: String, storage: Storage, value: V) {
-        self.identifier = identifier
-        self.storage = storage
-        self.value = value
-    }
-
-    public func addObserver(_ observer: AnyObject, onUpdated: @escaping (V) -> Void) {
-        notifier.addObserver(observer, onNotify: onUpdated)
-    }
-
-    public func removeObserver(_ observer: AnyObject) {
-        notifier.removeObserver(observer)
-    }
-}
-
-public class BooleanSetting: SettingImpl<Bool> {}
-
-
-
-public class BooleanSetting<Storage: SettingStorage>: Setting where Storage.ValueType == Bool {
-    private let identifier: String
-    private let storage: Storage
+    private let storage: any BooleanSettingStorage
+    private let updateNotifier: any BooleanSettingUpdateNotifier
 
     public var value: Bool {
         didSet {
             storage.store(value, for: identifier)
-            // notify
+            updateNotifier.notify(value)
         }
     }
 
-    public init(identifier: String, storage: Storage, defaultValue: Bool) {
+    public init(
+        identifier: String,
+        storage: any BooleanSettingStorage,
+        updateNotifier: any BooleanSettingUpdateNotifier,
+        defaultValue: Bool) {
         self.identifier = identifier
         self.storage = storage
+        self.updateNotifier = updateNotifier
         value = storage.value(for: identifier) ?? defaultValue
     }
 
-    public func addObserver(_ observer: AnyObject, onUpdated: (Bool) -> Void) {
-
+    public func addObserver(_ observer: AnyObject, onUpdated: @escaping (Bool) -> Void) {
+        updateNotifier.addObserver(observer, onNotify: onUpdated)
     }
 
     public func removeObserver(_ observer: AnyObject) {
-
+        updateNotifier.removeObserver(observer)
     }
 }
-
-public protocol BooleanSettingStorage: SettingStorage where ValueType == Bool {}
 
 class UserDefaultsBooleanSettingStorage: BooleanSettingStorage {
     func value(for identifier: String) -> Bool? {
-        return true
+        return UserDefaults.standard.object(forKey: identifier) as? Bool
     }
 
     func store(_ value: Bool, for identifier: String) {
-
+        UserDefaults.standard.set(value, forKey: identifier)
     }
 }
+
+extension ObserverNotifierImpl: BooleanSettingUpdateNotifier where Notification == Bool {}
+extension ObserverNotifierMainThreadDecorator: BooleanSettingUpdateNotifier where Notification == Bool {}
 
 public enum AppSettings {
     public static let boolSetting = BooleanSetting(
         identifier: "",
         storage: UserDefaultsBooleanSettingStorage(),
+        updateNotifier: ObserverNotifierMainThreadDecorator(notifier: ObserverNotifierImpl<Bool>()),
         defaultValue: true
     )
 }
-// need to have singleton!? make module AppSettings, def enum Settings, add as static properties
+
