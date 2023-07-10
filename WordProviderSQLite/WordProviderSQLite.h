f0224985bd3ea28b5e50695873c20103bb5ca549
Eigo Madaloja <eigo.madaloja@gmail.com>
2023-07-10 21:58:15 +0300

    added WordProviderSQLite

---
+++ WordProviderSQLite/WordProviderSQLite.h
@@ -0,0 +1,18 @@
+//
+//  WordProviderSQLite.h
+//  WordProviderSQLite
+//
+//  Created by Eigo Madaloja on 10.07.2023.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for WordProviderSQLite.
+FOUNDATION_EXPORT double WordProviderSQLiteVersionNumber;
+
+//! Project version string for WordProviderSQLite.
+FOUNDATION_EXPORT const unsigned char WordProviderSQLiteVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <WordProviderSQLite/PublicHeader.h>
+
+
