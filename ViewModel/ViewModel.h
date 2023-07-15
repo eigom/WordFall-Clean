49616dd4bd351aed8955ea99b1772969d3e3e379
Eigo Madaloja <eigo.madaloja@gmail.com>
2023-07-15 21:32:39 +0300

    added ViewModel

---
+++ ViewModel/ViewModel.h
@@ -0,0 +1,18 @@
+//
+//  ViewModel.h
+//  ViewModel
+//
+//  Created by Eigo Madaloja on 15.07.2023.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for ViewModel.
+FOUNDATION_EXPORT double ViewModelVersionNumber;
+
+//! Project version string for ViewModel.
+FOUNDATION_EXPORT const unsigned char ViewModelVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ViewModel/PublicHeader.h>
+
+
