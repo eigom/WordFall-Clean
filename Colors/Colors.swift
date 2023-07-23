e1a3b4417627cc90e320baf14135b4650997e1c9
Eigo Madaloja <eigo.madaloja@gmail.com>
2023-07-23 16:26:06 +0300

    added Colors

---
+++ Colors/Colors.swift
@@ -0,0 +1,134 @@
+// swiftlint:disable all
+// Generated using SwiftGen — https://github.com/SwiftGen/SwiftGen
+
+#if os(macOS)
+  import AppKit
+#elseif os(iOS)
+  import UIKit
+#elseif os(tvOS) || os(watchOS)
+  import UIKit
+#endif
+#if canImport(SwiftUI)
+  import SwiftUI
+#endif
+
+// Deprecated typealiases
+@available(*, deprecated, renamed: "ColorAsset.Color", message: "This typealias will be removed in SwiftGen 7.0")
+public typealias AssetColorTypeAlias = ColorAsset.Color
+
+// swiftlint:disable superfluous_disable_command file_length implicit_return
+
+// MARK: - Asset Catalogs
+
+// swiftlint:disable identifier_name line_length nesting type_body_length type_name
+public enum Color {
+  public enum Button {
+    public enum Next {
+      public static let background = ColorAsset(name: "Button/Next/background")
+    }
+    public enum Restore {
+      public static let background = ColorAsset(name: "Button/Restore/background")
+    }
+    public enum Solve {
+      public static let background = ColorAsset(name: "Button/Solve/background")
+    }
+    public static let smallTitle = ColorAsset(name: "Button/smallTitle")
+    public static let subtitle = ColorAsset(name: "Button/subtitle")
+    public static let title = ColorAsset(name: "Button/title")
+  }
+  public enum Definition {
+    public static let text = ColorAsset(name: "Definition/text")
+    public static let type = ColorAsset(name: "Definition/type")
+  }
+  public enum GamePlay {
+    public static let blue = ColorAsset(name: "GamePlay/blue")
+    public static let green = ColorAsset(name: "GamePlay/green")
+    public static let letter = ColorAsset(name: "GamePlay/letter")
+    public static let red = ColorAsset(name: "GamePlay/red")
+  }
+  public enum Option {
+    public static let selected = ColorAsset(name: "Option/selected")
+    public static let title = ColorAsset(name: "Option/title")
+    public static let unselected = ColorAsset(name: "Option/unselected")
+  }
+  public static let separator = ColorAsset(name: "separator")
+}
+// swiftlint:enable identifier_name line_length nesting type_body_length type_name
+
+// MARK: - Implementation Details
+
+public final class ColorAsset {
+  public fileprivate(set) var name: String
+
+  #if os(macOS)
+  public typealias Color = NSColor
+  #elseif os(iOS) || os(tvOS) || os(watchOS)
+  public typealias Color = UIColor
+  #endif
+
+  @available(iOS 11.0, tvOS 11.0, watchOS 4.0, macOS 10.13, *)
+  public private(set) lazy var color: Color = {
+    guard let color = Color(asset: self) else {
+      fatalError("Unable to load color asset named \(name).")
+    }
+    return color
+  }()
+
+  #if os(iOS) || os(tvOS)
+  @available(iOS 11.0, tvOS 11.0, *)
+  public func color(compatibleWith traitCollection: UITraitCollection) -> Color {
+    let bundle = BundleToken.bundle
+    guard let color = Color(named: name, in: bundle, compatibleWith: traitCollection) else {
+      fatalError("Unable to load color asset named \(name).")
+    }
+    return color
+  }
+  #endif
+
+  #if canImport(SwiftUI)
+  @available(iOS 13.0, tvOS 13.0, watchOS 6.0, macOS 10.15, *)
+  public private(set) lazy var swiftUIColor: SwiftUI.Color = {
+    SwiftUI.Color(asset: self)
+  }()
+  #endif
+
+  fileprivate init(name: String) {
+    self.name = name
+  }
+}
+
+public extension ColorAsset.Color {
+  @available(iOS 11.0, tvOS 11.0, watchOS 4.0, macOS 10.13, *)
+  convenience init?(asset: ColorAsset) {
+    let bundle = BundleToken.bundle
+    #if os(iOS) || os(tvOS)
+    self.init(named: asset.name, in: bundle, compatibleWith: nil)
+    #elseif os(macOS)
+    self.init(named: NSColor.Name(asset.name), bundle: bundle)
+    #elseif os(watchOS)
+    self.init(named: asset.name)
+    #endif
+  }
+}
+
+#if canImport(SwiftUI)
+@available(iOS 13.0, tvOS 13.0, watchOS 6.0, macOS 10.15, *)
+public extension SwiftUI.Color {
+  init(asset: ColorAsset) {
+    let bundle = BundleToken.bundle
+    self.init(asset.name, bundle: bundle)
+  }
+}
+#endif
+
+// swiftlint:disable convenience_type
+private final class BundleToken {
+  static let bundle: Bundle = {
+    #if SWIFT_PACKAGE
+    return Bundle.module
+    #else
+    return Bundle(for: BundleToken.self)
+    #endif
+  }()
+}
+// swiftlint:enable convenience_type
--- Texts/Texts.swift
+++ Texts/Texts.swift
@@ -9,85 +9,85 @@ import Foundation
 
 // swiftlint:disable explicit_type_interface function_parameter_count identifier_name line_length
 // swiftlint:disable nesting type_body_length type_name vertical_whitespace_opening_braces
-public enum Texts {
+public enum Text {
   public enum Alert {
     /// Word Fall
-    public static let defaultTitle = Texts.tr("Localizable", "alert.defaultTitle", fallback: "Word Fall")
+    public static let defaultTitle = Text.tr("Localizable", "alert.defaultTitle", fallback: "Word Fall")
     public enum Action {
       /// Cancel
-      public static let cancel = Texts.tr("Localizable", "alert.action.cancel", fallback: "Cancel")
+      public static let cancel = Text.tr("Localizable", "alert.action.cancel", fallback: "Cancel")
       /// OK
-      public static let dismiss = Texts.tr("Localizable", "alert.action.dismiss", fallback: "OK")
+      public static let dismiss = Text.tr("Localizable", "alert.action.dismiss", fallback: "OK")
     }
   }
   public enum Gameplay {
     public enum Intro {
       /// WordFall
-      public static let word = Texts.tr("Localizable", "gameplay.intro.word", fallback: "WordFall")
+      public static let word = Text.tr("Localizable", "gameplay.intro.word", fallback: "WordFall")
       public enum Definition {
         /// a word game where you form words and names from falling letters
-        public static let text = Texts.tr("Localizable", "gameplay.intro.definition.text", fallback: "a word game where you form words and names from falling letters")
+        public static let text = Text.tr("Localizable", "gameplay.intro.definition.text", fallback: "a word game where you form words and names from falling letters")
         /// noun
-        public static let type = Texts.tr("Localizable", "gameplay.intro.definition.type", fallback: "noun")
+        public static let type = Text.tr("Localizable", "gameplay.intro.definition.type", fallback: "noun")
       }
     }
     public enum NextButton {
       /// NEXT
-      public static let title = Texts.tr("Localizable", "gameplay.nextButton.title", fallback: "NEXT")
+      public static let title = Text.tr("Localizable", "gameplay.nextButton.title", fallback: "NEXT")
     }
     public enum PurchaseAlert {
       /// %@
       public static func title(_ p1: Any) -> String {
-        return Texts.tr("Localizable", "gameplay.purchaseAlert.title", String(describing: p1), fallback: "%@")
+        return Text.tr("Localizable", "gameplay.purchaseAlert.title", String(describing: p1), fallback: "%@")
       }
       public enum PurchaseAction {
         /// Get for %@
         public static func title(_ p1: Any) -> String {
-          return Texts.tr("Localizable", "gameplay.purchaseAlert.purchaseAction.title", String(describing: p1), fallback: "Get for %@")
+          return Text.tr("Localizable", "gameplay.purchaseAlert.purchaseAction.title", String(describing: p1), fallback: "Get for %@")
         }
       }
     }
     public enum RevealButton {
       /// REVEAL
-      public static let title = Texts.tr("Localizable", "gameplay.revealButton.title", fallback: "REVEAL")
+      public static let title = Text.tr("Localizable", "gameplay.revealButton.title", fallback: "REVEAL")
       public enum Purchase {
         /// get for %@
         public static func subtitle(_ p1: Any) -> String {
-          return Texts.tr("Localizable", "gameplay.revealButton.purchase.subtitle", String(describing: p1), fallback: "get for %@")
+          return Text.tr("Localizable", "gameplay.revealButton.purchase.subtitle", String(describing: p1), fallback: "get for %@")
         }
       }
       public enum RevealsLeft {
         /// %@ left
         public static func subtitle(_ p1: Any) -> String {
-          return Texts.tr("Localizable", "gameplay.revealButton.revealsLeft.subtitle", String(describing: p1), fallback: "%@ left")
+          return Text.tr("Localizable", "gameplay.revealButton.revealsLeft.subtitle", String(describing: p1), fallback: "%@ left")
         }
       }
     }
     public enum SettingsButton {
       /// ⦿
-      public static let title = Texts.tr("Localizable", "gameplay.settingsButton.title", fallback: "⦿")
+      public static let title = Text.tr("Localizable", "gameplay.settingsButton.title", fallback: "⦿")
     }
   }
   public enum Settings {
     public enum RestorePurchasesButton {
       /// restoring...
-      public static let restoringTitle = Texts.tr("Localizable", "settings.restorePurchasesButton.restoringTitle", fallback: "restoring...")
+      public static let restoringTitle = Text.tr("Localizable", "settings.restorePurchasesButton.restoringTitle", fallback: "restoring...")
       /// Restore purchase
-      public static let title = Texts.tr("Localizable", "settings.restorePurchasesButton.title", fallback: "Restore purchase")
+      public static let title = Text.tr("Localizable", "settings.restorePurchasesButton.title", fallback: "Restore purchase")
     }
     public enum Sound {
       /// Off
-      public static let off = Texts.tr("Localizable", "settings.sound.off", fallback: "Off")
+      public static let off = Text.tr("Localizable", "settings.sound.off", fallback: "Off")
       /// On
-      public static let on = Texts.tr("Localizable", "settings.sound.on", fallback: "On")
+      public static let on = Text.tr("Localizable", "settings.sound.on", fallback: "On")
       /// SOUND
-      public static let title = Texts.tr("Localizable", "settings.sound.title", fallback: "SOUND")
+      public static let title = Text.tr("Localizable", "settings.sound.title", fallback: "SOUND")
     }
     public enum WordLength {
       /// All
-      public static let all = Texts.tr("Localizable", "settings.wordLength.all", fallback: "All")
+      public static let all = Text.tr("Localizable", "settings.wordLength.all", fallback: "All")
       /// WORD LENGTH
-      public static let title = Texts.tr("Localizable", "settings.wordLength.title", fallback: "WORD LENGTH")
+      public static let title = Text.tr("Localizable", "settings.wordLength.title", fallback: "WORD LENGTH")
     }
   }
 }
@@ -96,7 +96,7 @@ public enum Texts {
 
 // MARK: - Implementation Details
 
-extension Texts {
+extension Text {
   private static func tr(_ table: String, _ key: String, _ args: CVarArg..., fallback value: String) -> String {
     let format = BundleToken.bundle.localizedString(forKey: key, value: value, table: table)
     return String(format: format, locale: Locale.current, arguments: args)
